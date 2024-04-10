40f52bd412be6211db2ef6d382a73bc66bd430aa
Mehmet676767 <162468939+Mehmet676767@users.noreply.github.com>
2024-04-10 19:19:24 +0300

    Update resume.py

--- FallenMusic/Modules/resume.py
+++ FallenMusic/Modules/resume.py
@@ -40,6 +40,6 @@ async def res_str(_, message: Message):
     await stream_on(message.chat.id)
     await pytgcalls.resume_stream(message.chat.id)
     return await message.reply_text(
-        text=f"➻ şarkı durdu💫\n│ \n└ʙʏ : {message.from_user.mention} 🥀",
+        text=f"➻ dewam ediyorum aşkolar💫\n│ \n└ʙʏ : {message.from_user.mention} 🥀",
         reply_markup=close_key,
     )
