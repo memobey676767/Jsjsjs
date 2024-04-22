c99f8f25c2fd49e945236200dc91dddcdf049f5d
Mehmet676767 <162468939+Mehmet676767@users.noreply.github.com>
2024-04-22 15:48:23 +0300

    Update watcher.py

--- FallenMusic/Modules/watcher.py
+++ FallenMusic/Modules/watcher.py
@@ -105,6 +105,6 @@ async def on_stream_end(pytgcalls, update: Update):
         await app.send_photo(
             chat_id=chat_id,
             photo=img,
-            caption=f"**ŞARKI BAŞLADI**\n\n‣ **BİLGİ :** [{title[:27]}](https://t.me/{BOT_USERNAME}?start=info_{videoid})\n‣ **⏳SÜRE :** `{duration}` DAKİKA\n‣ **TALEP EDEN :** {req_by}",
+            caption=f"**şarkı başladı**\n\n‣ **bilgi :** [{title[:27]}](https://t.me/{BOT_USERNAME}?start=info_{videoid})\n‣ **süre :** `{duration}` dakika\n‣ **talep eden :** {req_by}",
             reply_markup=buttons,
         )
