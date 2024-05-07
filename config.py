fe616af39e4520108763c541b95498b58b8b12b8
Mehmet676767 <162468939+Mehmet676767@users.noreply.github.com>
2024-05-08 01:39:49 +0300

    Update config.py

--- config.py
+++ config.py
@@ -14,7 +14,7 @@ DURATION_LIMIT = int(getenv("DURATION_LIMIT", "200"))
 OWNER_ID = int(getenv("OWNER_ID"))
 
 PING_IMG = getenv("PING_IMG", "https://telegra.ph/Bilmem-04-28")
-START_IMG = getenv("START_IMG", "https://telegra.ph/Bilmem-04-28")
+START_IMG = getenv("START_IMG", "https://telegra.ph/Biliyorum-05-07")
 
 SESSION = getenv("SESSION", None)
 
