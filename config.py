1f4dae0b4c427d731b9d0b799284cd78f3109382
Mehmet676767 <162468939+Mehmet676767@users.noreply.github.com>
2024-06-03 08:17:46 +0300

    Update config.py

--- config.py
+++ config.py
@@ -13,12 +13,12 @@ DURATION_LIMIT = int(getenv("DURATION_LIMIT", "200"))
 
 OWNER_ID = int(getenv("OWNER_ID"))
 
-PING_IMG = getenv("PING_IMG", "https://telegra.ph/Bilmem-04-28")
-START_IMG = getenv("START_IMG", "https://telegra.ph/Biliyorum-05-07")
+PING_IMG = getenv("PING_IMG", "https://telegra.ph/Baylar-04-13")
+START_IMG = getenv("START_IMG", "https://telegra.ph/Baylar-04-13")
 
 SESSION = getenv("SESSION", None)
 
-SUPPORT_CHAT = getenv("SUPPORT_CHAT", "https://t.me/sehrinanem")
+SUPPORT_CHAT = getenv("SUPPORT_CHAT", "https://t.me/masaldestek")
 SUPPORT_CHANNEL = getenv("SUPPORT_CHANNEL", "https://t.me/muzik_kolik")
 
 SUDO_USERS = list(map(int, getenv("SUDO_USERS", "1356469075").split()))
